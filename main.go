fd37037aa76f984ed75c4f40dd60e450128b56f3
tumpa02 <jtumpa@gmail.com>
2024-03-03 23:50:30 +0100

    feat: Create bot connection from BOT_TOKEN in .env. Simple ping-pong message function.

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+	"gomelody/pkg/messageCreate"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	Token string
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	Token = os.Getenv("BOT_TOKEN")
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("Error creating Discord session: ", err)
+		return
+	}
+
+	dg.AddHandler(messageCreate.MessageCreate)
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("Error opening connection: ", err)
+		return
+	}
+
+	fmt.Println("Bot is now running. Press CTRL+C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	err = dg.Close()
+	if err != nil {
+		return
+	}
 }
